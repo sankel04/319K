ae1ac598e0a8900b3ee04e0038c47cf168299c97
sankel04 <158630117+sankel04@users.noreply.github.com>
2024-04-23 14:48:01 -0500

    Add files via upload

--- Lab9Main.c
+++ Lab9Main.c
@@ -25,11 +25,12 @@ void PLL_Init(void){ // set phase lock loop (PLL)
   Clock_Init80MHz(0);   // run this line for 80MHz
 }
 
-
+uint32_t killedEnemys = 0;
+uint32_t delayTime = 0;
 
 typedef enum {English, Spanish} Language_t;
 Language_t myLanguage=English;
-typedef enum {HELLO, GOODBYE, LANGUAGE, WELCOME,INSTRUCTION1, INSTRUCTION2,OUTRO} phrase_t;
+typedef enum {HELLO, GOODBYE, LANGUAGE, WELCOME,INSTRUCTION1, INSTRUCTION2,OutroLose, OutroWin,PAUSE,RESTART} phrase_t;
 const char Hello_English[] ="Hello";
 const char Hello_Spanish[] ="\xADHola!";
 const char Goodbye_English[]="Goodbye";
@@ -42,16 +43,27 @@ const char Instruction1_English[] ="> fire missile";
 const char Instruction2_English[] ="< play/pause";
 const char Instruction1_Spanish[] ="> dispara el misil";
 const char Instruction2_Spanish[] ="< jugar/pausa";
-const char Outro_English[] ="GAME OVER\n Nice Try \n Earthling";
-const char Outro_Spanish[] ="Juego terminado\n Buen intento \n terricola";
-const char *Phrases[7][2]={
+const char OutroLose_English[] ="GAME OVER\n Nice Try \n Earthling";
+const char OutroLose_Spanish[] ="Juego terminado\n Buen intento \n terricola";
+const char OutroWin_English[] ="Great Job!\n Earthling";
+const char OutroWin_Spanish[] ="Buen trabajo!\n terricola";
+const char Pause_English[] ="pause";
+const char Pause_Spanish[] ="pausa";
+const char Restart_English[] ="< restart";
+const char Restart_Spanish[] ="< reanudar";
+
+
+const char *Phrases[10][2]={
   {Hello_English,Hello_Spanish},
   {Goodbye_English,Goodbye_Spanish},
   {Language_English,Language_Spanish},
    {Welcome_English,Welcome_Spanish},
    {Instruction1_English,Instruction1_Spanish},
    {Instruction2_English,Instruction2_Spanish},
-   {Outro_English,Outro_Spanish}
+   {OutroLose_English,OutroLose_Spanish},
+  {OutroWin_English,OutroWin_Spanish},
+  {Pause_English,Pause_Spanish},
+  {Restart_English,Restart_Spanish}
 };
 
 
@@ -92,10 +104,11 @@ uint32_t playerCNT;
 
 void Move(void){
   uint32_t i;
+  delayTime++;
   for(i=0; i<6; i++){
       if(Enemys[i].life == alive){
          NeedToDraw = 1; // mark as changed
-         if(Enemys[i].y < 142){
+         if(Enemys[i].y < 144){
             Enemys[i].oldY = Enemys[i].y;
             Enemys[i].oldX = Enemys[i].x;
             Enemys[i].y += 2; // move down
@@ -104,20 +117,28 @@ void Move(void){
             player.life = dead; // at bottom
          }
          for(uint8_t cnt = 0; cnt<playerCNT;cnt++){ // iterate through all player bullets and check against all alive enemies
-             if(playerBullets[cnt].y == (Enemys[i].y-2)){ // if player bullet made it to enemy y
-                 //if(((playerBullets[cnt].x+2) > (Enemys[i].x + 5)) && ((playerBullets[cnt].x+2) < (Enemys[i].x +14))){
-                     Enemys[i].life = dead;
+             if(playerBullets[cnt].life == alive){
+                 if((playerBullets[cnt].y+2) <= (Enemys[i].y+2)){ // if player bullet made it to enemy y
+                     if(((playerBullets[cnt].x+2) >= (Enemys[i].x + 2)) && ((playerBullets[cnt].x+2) <= (Enemys[i].x +14))){
+                         Enemys[i].life = dead;
+                         Sound_Killed();
+                         playerBullets[cnt].life = dead;
+                         killedEnemys++;
 
-                 //}
+                     }
+                 }
              }
          }
       }
   }
   for(uint8_t i=0;i<20;i++){ // iterate through all bullets and move them down / up
       NeedToDraw = 1;
-      if(Bullets[i].y >= 142){ // checks if bullet at bottom
-          if(((Bullets[i].x + 2) > (player.x - 7)) && ((Bullets[i].x + 2) < (player.x +18))){ // checks if bullets in range of player
-              player.life = dead;
+      if(Bullets[i].life == alive){
+          if((Bullets[i].y >= 142) && (Bullets[i].y <= 148)){ // checks if bullet at bottom
+              if(((Bullets[i].x + 2) > (player.x - 7)) && ((Bullets[i].x + 2) < (player.x +18))){ // checks if bullets in range of player
+                  player.life = dead;
+                  Bullets[i].life = dead;
+              }
           }
       }
       Bullets[i].oldY = Bullets[i].y;
@@ -130,28 +151,48 @@ void Move(void){
 }
 uint32_t enemyCNT;
 void genBullets(void){
+    Sound_Shoot();
     enemyCNT++;
     uint8_t ran = Random(6); // gets random 0-5
     // adds random placed bullet to bullet array
-    Bullets[bulletIndex].x = Enemys[ran].x;
-    Bullets[bulletIndex].y = Enemys[ran].y;
-    Bullets[bulletIndex].oldX = Enemys[ran].oldX;
-    Bullets[bulletIndex].oldY = Enemys[ran].oldY;
-    Bullets[bulletIndex].image = Bullet;
-    Bullets[bulletIndex].life = alive;
-    bulletIndex = (bulletIndex+1)%20; // keeps index in range 0-19
+    if(Enemys[ran].life == alive){
+        Bullets[bulletIndex].x = Enemys[ran].x + 7;
+        Bullets[bulletIndex].y = Enemys[ran].y + 7;
+        Bullets[bulletIndex].oldX = Enemys[ran].oldX;
+        Bullets[bulletIndex].oldY = Enemys[ran].oldY;
+        Bullets[bulletIndex].image = Bullet;
+        Bullets[bulletIndex].life = alive;
+        bulletIndex = (bulletIndex+1)%20; // keeps index in range 0-19
+    }
 }
 
 void genPlayerBullet(void){
+    Sound_Shoot();
     playerCNT++;
-    playerBullets[playerBulletIndex].x = player.x;
-    playerBullets[playerBulletIndex].y = player.y;
+    playerBullets[playerBulletIndex].x = player.x + 8;
+    playerBullets[playerBulletIndex].y = player.y - 5;
     playerBullets[playerBulletIndex].oldX = player.x; //playerBullets[playerBulletIndex].x;
     playerBullets[playerBulletIndex].oldY = player.y; //playerBullets[playerBulletIndex].y;
     playerBullets[playerBulletIndex].image = playerBullet;
     playerBullets[playerBulletIndex].life = alive;
     playerBulletIndex = (playerBulletIndex+1)%20; // keeps index in range 0-19
 }
+
+void Enemy_Init(void){ int i;
+    for(i=0; i<6; i++){
+        Enemys[i].x = 20*i+12;
+        Enemys[i].y = 10; // along the top
+        Enemys[i].image = SmallEnemy30pointA;
+        Enemys[i].life = alive;
+    }
+    Enemys[0].oldX = 0; Enemys[0].oldY = 0;
+    Enemys[1].oldX = 0; Enemys[1].oldY = 0;
+    Enemys[2].oldX = 0; Enemys[2].oldY = 0;
+    Enemys[3].oldX = 0; Enemys[3].oldY = 0;
+    Enemys[4].oldX = 0; Enemys[4].oldY = 0;
+    Enemys[5].oldX = 0; Enemys[5].oldY = 0;
+}
+
 void EdgeTriggered_Init(void){
     //LaunchPad_Init(); // PB21 is input with internal pull up resistor
     GPIOB->POLARITY15_0 = 0x08000000; // falling
@@ -169,33 +210,116 @@ void EdgeTriggered_Init(void){
 }
 
 void GROUP1_IRQHandler(void){
-    LED_Toggle(2);
     GPIOB->CPU_INT.ICLR = 0x002000; // clear bit 13
-    genPlayerBullet();
+    if(delayTime >= 2){
+        delayTime = 0;
+        genPlayerBullet();
+    }
+
 }
 
 
 
 uint8_t Flag = 0; // global semaphore
-
+uint32_t counter = 0;
+uint32_t intCount = 0;
 // games  engine runs at 30Hz
 void TIMG12_IRQHandler(void){uint32_t pos,msg;
   if((TIMG12->CPU_INT.IIDX) == 1){ // this will acknowledge
     GPIOB->DOUTTGL31_0 = GREEN; // toggle PB27 (minimally intrusive debugging)
     GPIOB->DOUTTGL31_0 = GREEN; // toggle PB27 (minimally intrusive debugging)
 // game engine goes here
+    if(Switch_In() == 1){
+      //  Clock_Delay1ms(100);
+        ST7735_SetCursor(1, 1);
+        ST7735_OutString((char *)Phrases[PAUSE][myLanguage]);
+        ST7735_OutChar(13);
+        ST7735_OutString((char *)Phrases[RESTART][myLanguage]);
+        while(1){
+            if(Switch_In() == 1){
+                ST7735_FillScreen(0x0000);
+                break;
+            }
+        }
+    }
+    intCount++;
+    if(intCount == 8){
+        intCount = 0;
+        Move(); // moves the enemy sprites
+        if(counter == 6){
+            genBullets();
+        }
+        counter = (counter+1)%7;
+    }
     if(player.life == dead){
+        Sound_Explosion();
         ST7735_FillScreen(ST7735_BLACK);
         ST7735_SetCursor(1, 1);
         ST7735_FillScreen(0x0000);   // set screen to black
-        ST7735_OutString((char *)Phrases[OUTRO][myLanguage]);
+        ST7735_OutString((char *)Phrases[OutroLose][myLanguage]);
         ST7735_SetCursor(2, 4);
-        ST7735_OutUDec(1234);
+        ST7735_OutUDec(killedEnemys);
         while(1){
+            Clock_Delay1ms(750);
+
+            if(Switch_In() == 1){
+                killedEnemys = 0;
+                for(uint8_t i = 0; i<20; i++){
+                    Bullets[i].life = dead;
+                    playerBullets[i].life = dead;
+                }
+                ST7735_FillScreen(ST7735_BLACK);
+                Enemy_Init();
+                player.life = alive;
+                Clock_Delay1ms(250);
+                LED_Toggle(1);
+                Clock_Delay1ms(250);
+                LED_Toggle(1);
+                LED_Toggle(2);
+                Clock_Delay1ms(250);
+                LED_Toggle(2);
+                LED_Toggle(3);
+                Clock_Delay1ms(250);
+                LED_Toggle(3);
+                break;
+            }
         } // infinite loop
     }
+    if(killedEnemys == 6){
+               ST7735_FillScreen(ST7735_BLACK);
+               ST7735_SetCursor(1, 1);
+               ST7735_FillScreen(0x0000);   // set screen to black
+               ST7735_OutString((char *)Phrases[OutroWin][myLanguage]);
+               ST7735_SetCursor(2,4);
+               ST7735_OutUDec(killedEnemys);
+               while(1){
+                   Clock_Delay1ms(750);
+                   if(Switch_In() == 1){
+                       killedEnemys = 0;
+                       for(uint8_t i = 0; i<20; i++){
+                           Bullets[i].life = dead;
+                           playerBullets[i].life = dead;
+                       }
+                       ST7735_FillScreen(ST7735_BLACK);
+                       Enemy_Init();
+                       player.life = alive;
+                       Clock_Delay1ms(250);
+                       LED_Toggle(1);
+                       Clock_Delay1ms(250);
+                       LED_Toggle(1);
+                       LED_Toggle(2);
+                       Clock_Delay1ms(250);
+                       LED_Toggle(2);
+                       LED_Toggle(3);
+                       Clock_Delay1ms(250);
+                       LED_Toggle(3);
+                       break;
+                   }
+               } // infinite loop
+           }
+
     uint32_t x = (120*ADCin())/4095; // 1) sample slide pot 0 to 120
-    uint8_t switchState = Switch_In(); // 2) read input switches
+    //uint8_t switchState = Switch_In(); // 2) read input switches
     // do an if pause/play statement here
     //Move(); // 3) move sprites
     if(x != player.x){
@@ -209,54 +333,14 @@ void TIMG12_IRQHandler(void){uint32_t pos,msg;
     GPIOB->DOUTTGL31_0 = GREEN; // toggle PB27 (minimally intrusive debugging)
   }
 }
-uint32_t counter = 0;
+
 void SysTick_Handler(void){ // called at 11 kHz
-  // output one value to DAC if a sound is active
-  Move(); // moves the enemy sprites
-  if(counter == 6){
-      genBullets();
-  }
-  counter = (counter+1)%7;
+  DAC5_Out(dacVal()); // output one value to DAC if a sound is active
 }
 
 uint8_t TExaS_LaunchPadLogicPB27PB26(void){
   return (0x80|((GPIOB->DOUT31_0>>26)&0x03));
 }
-
-
-
-void Enemy_Init(void){ int i;
-    for(i=0; i<6; i++){
-        Enemys[i].x = 20*i+12;
-        Enemys[i].y = 10; // along the top
-        Enemys[i].image = SmallEnemy30pointA;
-        Enemys[i].life = alive;
-    }
-    /*
-    for(i=0; i<6; i++){
-         Bullets[i].x = 20*i+16;
-         Bullets[i].y = 10; // along the top
-         Bullets[i].image = Bullet;
-         Bullets[i].life = alive;
-    }
-*/
-    Enemys[0].oldX = 0; Enemys[0].oldY = 0;
-    Enemys[1].oldX = 0; Enemys[1].oldY = 0;
-    Enemys[2].oldX = 0; Enemys[2].oldY = 0;
-    Enemys[3].oldX = 0; Enemys[3].oldY = 0;
-    Enemys[4].oldX = 0; Enemys[4].oldY = 0;
-    Enemys[5].oldX = 0; Enemys[5].oldY = 0;
-/*
-    Bullets[0].oldX = 0; Bullets[0].oldY = 0;
-    Bullets[1].oldX = 0; Bullets[1].oldY = 0;
-    Bullets[2].oldX = 0; Bullets[2].oldY = 0;
-    Bullets[3].oldX = 0; Bullets[3].oldY = 0;
-    Bullets[4].oldX = 0; Bullets[4].oldY = 0;
-    Bullets[5].oldX = 0; Bullets[5].oldY = 0;
-*/
-}
-
-
 
 // use main1 to observe special characters
 int main1(void){ // main1
@@ -331,7 +415,7 @@ int main2(void){ // main2
   }
   ST7735_FillScreen(0x0000);   // set screen to black
   ST7735_SetCursor(1, 1);
-  ST7735_OutString((char *)Phrases[OUTRO][myLanguage]);
+  //ST7735_OutString((char *)Phrases[OUTRO][myLanguage]);
   ST7735_SetCursor(2, 4);
   ST7735_OutUDec(1234);
   while(1){
@@ -421,6 +505,8 @@ int main(void){ // final main
    ST7735_OutChar(31);
    ST7735_OutChar(13);
    Switch_Init();
+   myLanguage = English;
+
    while(1){
        if(Switch_In() == 3){ // up
            myLanguage = English;
@@ -439,7 +525,7 @@ int main(void){ // final main
      ST7735_OutString((char *)Phrases[HELLO][myLanguage]);
      ST7735_OutChar(13);
      ST7735_OutString((char *)Phrases[WELCOME][myLanguage]);
-     Clock_Delay1ms(3000);
+     Clock_Delay1ms(1500);
      ST7735_FillScreen(0x0000);
      ST7735_SetCursor(0,0);
      ST7735_OutString((char *)Phrases[INSTRUCTION1][myLanguage]);
@@ -450,7 +536,8 @@ int main(void){ // final main
              break;
          }
      }
-     while(1){
+
+
             // write code to test switches and LEDs
               Clock_Delay1ms(250);
               LED_Toggle(1);
@@ -462,8 +549,7 @@ int main(void){ // final main
               LED_Toggle(3);
               Clock_Delay1ms(250);
               LED_Toggle(3);
-              break;
-         }
+
   __enable_irq();
   ST7735_FillScreen(ST7735_BLACK);
   while(1){
@@ -481,13 +567,16 @@ int main(void){ // final main
 
               }
               for(uint8_t i = 0; i<=playerCNT; i++){
-
-                  ST7735_DrawBitmap(playerBullets[i].oldX, playerBullets[i].oldY, BlankBullet, 5,8);
-                  ST7735_DrawBitmap(playerBullets[i].x, playerBullets[i].y, playerBullet, 5,8);
+                  if(playerBullets[i].life == alive){
+                      ST7735_DrawBitmap(playerBullets[i].oldX, playerBullets[i].oldY, BlankBullet, 5,8);
+                      ST7735_DrawBitmap(playerBullets[i].x, playerBullets[i].y, playerBullet, 5,8);
+                  }
               }
               for(uint8_t i = 0; i<=enemyCNT; i++){
-                  ST7735_DrawBitmap(Bullets[i].oldX, Bullets[i].oldY, BlankBullet, 5,8);
-                  ST7735_DrawBitmap(Bullets[i].x, Bullets[i].y, Bullet, 5,8);
+                  if(Bullets[i].life == alive){
+                      ST7735_DrawBitmap(Bullets[i].oldX, Bullets[i].oldY, BlankBullet, 5,8);
+                      ST7735_DrawBitmap(Bullets[i].x, Bullets[i].y, Bullet, 5,8);
+                  }
               }
 
           }
@@ -495,4 +584,3 @@ int main(void){ // final main
       }
   }
 }
-
